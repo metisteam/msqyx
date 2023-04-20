78b311e5764dfb3e1d04387e1c1f64d716666f4a
tianhesong <419097924@qq.com>
2023-04-21 00:09:25 +0800

    333

--- cloud/config/main-local.php
+++ cloud/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'owxpkw7CyGby2cuFX2BFlJf61aK7KAUZ',
+            'cookieValidationKey' => 'fxtR_y5DJtNupmO37lp733h87HTvuixX',
         ],
     ],
 ];
