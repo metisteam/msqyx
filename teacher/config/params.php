15383c41f08a6b6b3031c35aab130cefa6a1948a
tianhesong <419097924@qq.com>
2023-06-21 14:23:40 +0800

    Update params.php
    去除客服电话

--- teacher/config/params.php
+++ teacher/config/params.php
@@ -2,84 +2,83 @@
 return [
     'adminEmail' => 'metisteam@meishuquan.net',
     //腾讯云信息
-    'teacherRole'  => 'teacher',
-    
-    'studentRole'  => 'student',
+    'teacherRole' => 'teacher',
 
-    'familyRole'   => 'family',
+    'studentRole' => 'student',
 
-    'downUrl'=> 'http://www.meishuquanyunxiao.com/download/index.html?token_value=',
+    'familyRole' => 'family',
+
+    'downUrl' => 'http://www.meishuquanyunxiao.com/download/index.html?token_value=',
 
     'DefautImage' => 'http://meishuquanyunxiao.img-cn-beijing.aliyuncs.com/icon/touxiang.png?x-oss-process=style/ef92587c6ac8577915de51f9fa6cae2c',
 
-    'UserSelectArray' => ['admin_id'=>'id','phone_number','studio_id','name','campus_id','class_id','usersig','gender','contact_phone','national_id','province','grade','school_name','graduation_at','total_score','image','token_value','is_image'],
+    'UserSelectArray' => ['admin_id' => 'id', 'phone_number', 'studio_id', 'name', 'campus_id', 'class_id', 'usersig', 'gender', 'contact_phone', 'national_id', 'province', 'grade', 'school_name', 'graduation_at', 'total_score', 'image', 'token_value', 'is_image'],
     'Category' => array(
-    			  1  => '成绩查询',
-    			  2  => '招生简章',
-    			  3  => '考点安排',
-    			  4  => '录取规则',
-    			  5  => '录取线',
-    			  7  => '招生计划',
-    			  9  => '历年考题',
-    			  10  => '专业介绍'
+        1 => '成绩查询',
+        2 => '招生简章',
+        3 => '考点安排',
+        4 => '录取规则',
+        5 => '录取线',
+        7 => '招生计划',
+        9 => '历年考题',
+        10 => '专业介绍'
     ),
 
     'Studio' => array(
-          43,
-          103,
-          183,
-          180
+        43,
+        103,
+        183,
+        180
     ),
 
     'FreeThreeMonth' => array(
-            379,
-            330,
-            348,
-            319,
-            353,
-            375,
-            318
-    ), 
+        379,
+        330,
+        348,
+        319,
+        353,
+        375,
+        318
+    ),
     'Shenfen' => array(
         "001",
         "007",
         "008"
     ),
     'NoYanZheng' => array(
-          103,
-          183
+        103,
+        183
     ),
     'OhterShenfen' => array(
         "001"
     ),
-    'KeFu'   => array(
-                      '17601099965'=>4028,
-                      '18601986083'=>13564,
-                      '18233228445'=>11319
+    'KeFu' => array(
+        '17601099965' => 4028,
+        '18601986083' => 13564
     ),
-    'NotUsers'  => array(
-                   '55555555',
-                   '66666666',
-                   '77777777',
-                   '88888888',
-                   '19870613',
-                   '2AA189FF',
-                   '13001277'
+    'NotUsers' => array(
+        '55555555',
+        '66666666',
+        '77777777',
+        '88888888',
+        '19870613',
+        '2AA189FF',
+        '13001277'
     ),
-    'DuanXin'   => array('p', '8', '0', '2', 'n', '4', '1', 'q', 'c', 's', '9', 'x', 'e',
-                                   'm', 'b', 'r', 'g', 'k', 'i', 'f', 'v', '7', 'w', 'o', 'd', 'j',
-                                   't', '6', 'z', 'h', 'a', 'l', '3', 'y', 'u', '5'),
+    'DuanXin' => array('p', '8', '0', '2', 'n', '4', '1', 'q', 'c', 's', '9', 'x', 'e',
+        'm', 'b', 'r', 'g', 'k', 'i', 'f', 'v', '7', 'w', 'o', 'd', 'j',
+        't', '6', 'z', 'h', 'a', 'l', '3', 'y', 'u', '5'),
 
-    'Version'   => 48,
-    'wx'=>[
+    'Version' => 48,
+    'wx' => [
         //  小程序配置
-        'mini'=>[
+        'mini' => [
             //  基本配置
-            'app_id' => 'wx9d7fc6d8fbc166e7', 
+            'app_id' => 'wx9d7fc6d8fbc166e7',
             'secret' => '3cf4f8769d8bd21707631b54546a705c',
             'token' => '',
-            'safeMode'=>0,
-            'encodingAESKey'=>'',
+            'safeMode' => 0,
+            'encodingAESKey' => '',
             //  微信支付
             // 'payment' => [
             //     'mch_id'        => '',
@@ -102,7 +101,7 @@ return [
         10 => 2698.00,
         11 => 2998.00,
         12 => 3998.00,
-        13 => 4398.00 
+        13 => 4398.00
     ],
     'IosPrice' => [
         0 => 0.00,
@@ -132,4 +131,4 @@ return [
     3.看到老师，点击直接联系即可
     我们在这等你呦！'
 
- ];
+];
