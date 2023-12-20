5e0a3d34ff0bc603fa273715171eff978c73ac40
Ez_Song <419097924@qq.com>
2023-12-20 12:42:22 +0800

    修改三个月账号的限制

--- common/models/ActivationCode.php
+++ common/models/ActivationCode.php
@@ -21,8 +21,8 @@ class ActivationCode extends ActiveRecord
     const USE_DELETED  = 20;
     const USE_ACTIVE   = 10;
     const TYPE_USER    = 2;
-    const TYPE_TEACHER = 1;  
-    const TYPE_FAMILY  = 3;  
+    const TYPE_TEACHER = 1;
+    const TYPE_FAMILY  = 3;
 
     /**
      * @inheritdoc
@@ -37,26 +37,27 @@ class ActivationCode extends ActiveRecord
             if ($this->isNewRecord) {
                 $this->created_at = time();
                 $this->updated_at = time();
+                $this->due_time   = $this->getEndTimeDate($this->created_at, $this->activetime);
                 /**
                  * 判断是否为183 否则三个月激活码时间为4月1或者5月1
                  */
 
-                if($this->activetime == 0.25) {
-                    if(in_array($this->studio_id,Yii::$app->params['Studio'])){
-                        $this->due_time   = $this->getEndTimeDate($this->created_at,$this->activetime);
-                    }else{
-                        if(in_array($this->studio_id,Yii::$app->params['FreeThreeMonth'])) {
-                            $this->due_time = "2021-06-11";
-                            $this->is_first = 1;
-                        }else{
-                            $this->due_time = "2021-05-01";
-                            $this->is_first = 1;
-                        }
-                    }
-                }else{
-                    $this->due_time   = $this->getEndTimeDate($this->created_at,$this->activetime);
-                }
-            }else{
+                // if ($this->activetime == 0.25) {
+                //     if (in_array($this->studio_id, Yii::$app->params['Studio'])) {
+                //         $this->due_time   = $this->getEndTimeDate($this->created_at, $this->activetime);
+                //     } else {
+                //         if (in_array($this->studio_id, Yii::$app->params['FreeThreeMonth'])) {
+                //             $this->due_time = "2021-06-11";
+                //             $this->is_first = 1;
+                //         } else {
+                //             $this->due_time = "2021-05-01";
+                //             $this->is_first = 1;
+                //         }
+                //     }
+                // } else {
+                //     $this->due_time   = $this->getEndTimeDate($this->created_at, $this->activetime);
+                // }
+            } else {
                 $this->updated_at = time();
             }
             return true;
@@ -67,16 +68,16 @@ class ActivationCode extends ActiveRecord
      * @inheritdoc
      */
     public function rules()
-    {   
+    {
         return [
-            [['code', 'type', 'studio_id','activetime','campus_id'], 'required','on'=>['create']],
-            [['status'],'required','on'=>['update']],
-            [['type', 'relation_id', 'studio_id', 'status','created_at','updated_at','is_active','activation_times'], 'integer'],
-            ['activetime','double'],
-            [['campus_id','class_id'], 'string'],
+            [['code', 'type', 'studio_id', 'activetime', 'campus_id'], 'required', 'on' => ['create']],
+            [['status'], 'required', 'on' => ['update']],
+            [['type', 'relation_id', 'studio_id', 'status', 'created_at', 'updated_at', 'is_active', 'activation_times'], 'integer'],
+            ['activetime', 'double'],
+            [['campus_id', 'class_id'], 'string'],
             ['status', 'in', 'range' => [self::STATUS_DELETED, self::STATUS_ACTIVE]],
-            ['activetime', 'in', 'range' => [0,1,2,3,0.019,0.09,0.25,0.5]],
-            ['type', 'in', 'range' => [self::TYPE_TEACHER,self::TYPE_USER]],
+            ['activetime', 'in', 'range' => [0, 1, 2, 3, 0.019, 0.09, 0.25, 0.5]],
+            ['type', 'in', 'range' => [self::TYPE_TEACHER, self::TYPE_USER]],
             ['is_active', 'in', 'range' => [self::USE_DELETED, self::USE_ACTIVE]],
             ['status', 'default', 'value' => self::STATUS_ACTIVE],
             ['activation_times', 'default', 'value' => 1],
@@ -103,32 +104,34 @@ class ActivationCode extends ActiveRecord
         ];
     }
 
-    public  function getEndTimeDate($time,$int) {
+    public  function getEndTimeDate($time, $int)
+    {
         $int = ($int == 0) ? 1 : $int;
-        if($int >= 1){
+        if ($int >= 1) {
             $year = floor($int);
-            $EndTime =  strtotime("+$year years +1 day" ,$time);
-        }elseif($int < 1 && $int >= 0.09){
-            $month =  floor($int*12);
-            $EndTime =  strtotime("+$month months +1 day",$time);
-        }else{
-            $days =  round($int*365)+1;
-            $EndTime =  strtotime("+$days days",$time);
+            $EndTime =  strtotime("+$year years +1 day", $time);
+        } elseif ($int < 1 && $int >= 0.09) {
+            $month =  floor($int * 12);
+            $EndTime =  strtotime("+$month months +1 day", $time);
+        } else {
+            $days =  round($int * 365) + 1;
+            $EndTime =  strtotime("+$days days", $time);
         }
-        return date("Y-m-d",$EndTime);
+        return date("Y-m-d", $EndTime);
     }
     /**
-    *[获取激活码到期时间]
-    *   
-    */
+     *[获取激活码到期时间]
+     *   
+     */
 
-    public static function GetUserEndTime($id, $type = 2) {
+    public static function GetUserEndTime($id, $type = 2)
+    {
 
-        $Code = self::findOne(['relation_id'=> $id , 'type' => $type]);
+        $Code = self::findOne(['relation_id' => $id, 'type' => $type]);
 
         $days = floor($Code->activetime * 365);
 
-        return floor((strtotime("+ $days days",$Code->created_at) - time())/(24*3500));
+        return floor((strtotime("+ $days days", $Code->created_at) - time()) / (24 * 3500));
     }
 
     /**
@@ -136,9 +139,9 @@ class ActivationCode extends ActiveRecord
      *
      *
      *
-    */
+     */
     public function getAdmins()
     {
-        return $this->hasOne(Admin::className(),['id'=>'relation_id'])->alias('admins');
+        return $this->hasOne(Admin::className(), ['id' => 'relation_id'])->alias('admins');
     }
 }
